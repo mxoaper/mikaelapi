87a252dbf418c4a935284f8605d8ac2ee0a35190
Mikael Nortikyan <nortikyan99@gmail.com>
2015-05-15 00:42:43 -0700

    Milestone 2

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
-
 //Configuration for our PHP Server
 set_time_limit(0);
-ini_set('default_socket_timeout', 300);
+int_set('default_soclet_timeout', 300);
+seesion_start();
 
-session_start();
-
-//Make Constant using define.
-define('clientID', '450fb4d9e03d4b80bc76515250409144');
-define('clientSecret', '682c5aa552c04c699a0c5fc588013947');
+//Make Constants using define
+define('client_id', '450fb4d9e03d4b80bc76515250409144');
+define('client_secret', '682c5aa552c04c699a0c5fc588013947');
 define('redirectURI', 'http://localhost/mikaelapi/index.php');
 define('ImageDirectory', 'pics/');
-
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
-	
-
-</body>
-</html>
+<!-- 
+Client INFO
+CLIENT ID	450fb4d9e03d4b80bc76515250409144
+CLIENT SECRET	682c5aa552c04c699a0c5fc588013947
+WEBSITE URL	http://localhost/mikaelapi/index.php
+REDIRECT URI	http://localhost/mikaelapi/index.php
+ -->
--- info.php
+++
@@ -1,4 +0,0 @@
-CLIENT ID	450fb4d9e03d4b80bc76515250409144
-CLIENT SECRET	682c5aa552c04c699a0c5fc588013947
-WEBSITE URL	http://localhost/mikaelapi/index.php
-REDIRECT URI	http://localhost/mikaelapi/index.php
