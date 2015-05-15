d4992f9ad75d05081667a84cdfecbd668d3be130
Mikael Nortikyan <nortikyan99@gmail.com>
2015-05-15 01:39:14 -0700

    Milestone 5
    Making an If else statement

--- index.php
+++ index.php
@@ -6,9 +6,22 @@ session_start();
 
 //Make Constants using define
 define('clientID', '450fb4d9e03d4b80bc76515250409144');
-define('client_ecret', '682c5aa552c04c699a0c5fc588013947');
+define('clienSecret', '682c5aa552c04c699a0c5fc588013947');
 define('redirectURI', 'http://localhost/mikaelapi/index.php');
 define('ImageDirectory', 'pics/');
+
+if isset(($GET['code'])) {
+    $code = ($GET['code']);
+    $url = 'https://api.instagram.com/oauth/access_token';
+    $access_token_settings = array('client_id' => clientID, 
+                                   'client_secret' => clientSecret,
+                                   'grant_type' => 'authorization_code',
+                                   'redirect_uri' => redirectURI,
+                                   'code' => $code
+                                    );
+}
+
+
 ?>
 
 <!DOCTYPE html>
