190b05fccb86d27d39fefb397e3638691bfe88eb
Mikael Nortikyan <nortikyan99@gmail.com>
2015-05-15 01:32:35 -0700

    Milestone 4
    Fixed the ERROR!!!

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define
-define('client_ID', '450fb4d9e03d4b80bc76515250409144');
+define('clientID', '450fb4d9e03d4b80bc76515250409144');
 define('client_ecret', '682c5aa552c04c699a0c5fc588013947');
+define('redirectURI', 'http://localhost/mikaelapi/index.php');
 define('ImageDirectory', 'pics/');
 ?>
 
@@ -19,7 +20,7 @@ define('ImageDirectory', 'pics/');
 	<body>
 		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account -->
 		<!-- After getting approval we are now going to have the information so that we can playu with this -->
-		<a href="https:api.instagram/oauth/authorize/>client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN!!!</a>
+		<a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
 		<script src="js/main.js"></script>
 	</body>
 </html>
