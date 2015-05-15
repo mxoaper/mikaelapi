6464d458f08fdc5c4bd04b97a5228b239fd13e91
Mikael Nortikyan <nortikyan99@gmail.com>
2015-05-15 01:01:26 -0700

    Milestone 3

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 //Configuration for our PHP Server
 set_time_limit(0);
-int_set('default_soclet_timeout', 300);
-seesion_start();
+ini_set('default_socket_timeout', 300);
+session_start();
 
 //Make Constants using define
-define('client_id', '450fb4d9e03d4b80bc76515250409144');
-define('client_secret', '682c5aa552c04c699a0c5fc588013947');
-define('redirectURI', 'http://localhost/mikaelapi/index.php');
+define('client_ID', '450fb4d9e03d4b80bc76515250409144');
+define('client_ecret', '682c5aa552c04c699a0c5fc588013947');
 define('ImageDirectory', 'pics/');
 ?>
 
-<!-- 
-Client INFO
-CLIENT ID	450fb4d9e03d4b80bc76515250409144
-CLIENT SECRET	682c5aa552c04c699a0c5fc588013947
-WEBSITE URL	http://localhost/mikaelapi/index.php
-REDIRECT URI	http://localhost/mikaelapi/index.php
- -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account -->
+		<!-- After getting approval we are now going to have the information so that we can playu with this -->
+		<a href="https:api.instagram/oauth/authorize/>client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN!!!</a>
+		<script src="js/main.js"></script>
+	</body>
+</html>
